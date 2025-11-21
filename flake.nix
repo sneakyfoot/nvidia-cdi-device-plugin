5f9f67d4cc50bc233838a2240655c21e94641f24
ez <evan_zazula@me.com>
2025-11-21 00:41:18 -0800

    init oom-gpu-operator

---
+++ flake.nix
@@ -0,0 +1,119 @@
+{
+  description = "Minimal NVIDIA CDI device plugin for Kubernetes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    systems = [ "x86_64-linux" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in
+  {
+    ###########################################################################
+    # PACKAGES (binary + OCI image)
+    ###########################################################################
+    packages = forAllSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        # ---- Binary ----
+        nvidia-cdi-device-plugin = pkgs.buildGoModule {
+          pname = "nvidia-cdi-device-plugin";
+          version = "0.1.0";
+
+          src = ./.;
+
+          vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          ldflags = [ "-s" "-w" ];
+        };
+
+        # ---- OCI container image ----
+        nvidia-cdi-device-plugin-image =
+          pkgs.dockerTools.buildImage {
+            name = "nvidia-cdi-device-plugin";
+            tag = "0.1.0";
+
+            contents = [
+              self.packages.${system}.nvidia-cdi-device-plugin
+            ];
+
+            config = {
+              Entrypoint = [ "/bin/nvidia-cdi-device-plugin" ];
+            };
+
+            extraCommands = ''
+              mkdir -p $out/bin
+              ln -s ${self.packages.${system}.nvidia-cdi-device-plugin}/bin/nvidia-cdi-device-plugin $out/bin/
+            '';
+          };
+      }
+    );
+
+    ###########################################################################
+    # DEV SHELL
+    ###########################################################################
+    devShells = forAllSystems (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.gopls
+            pkgs.go-tools
+          ];
+        };
+      }
+    );
+
+    ###########################################################################
+    # NixOS HOST MODULE (optional)
+    ###########################################################################
+    nixosModules.nvidia-cdi-device-plugin = { config, pkgs, lib, ... }:
+    let
+      cfg = config.services.nvidiaCdiDevicePlugin;
+      inherit (lib) mkEnableOption mkIf mkOption types;
+    in
+    {
+      options.services.nvidiaCdiDevicePlugin = {
+        enable = mkEnableOption "NVIDIA CDI Kubernetes device plugin";
+
+        package = mkOption {
+          type = types.package;
+          default = self.packages.${pkgs.system}.nvidia-cdi-device-plugin;
+        };
+
+        resourceName = mkOption {
+          type = types.str;
+          default = "nvidia.com/gpu";
+        };
+
+        kubeletDevicePluginDir = mkOption {
+          type = types.path;
+          default = "/var/lib/kubelet/device-plugins";
+        };
+      };
+
+      config = mkIf cfg.enable {
+        systemd.services.nvidia-cdi-device-plugin = {
+          description = "NVIDIA CDI device plugin for Kubernetes";
+
+          wants    = [ "kubelet.service" ];
+          after    = [ "kubelet.service" "network-online.target" ];
+          wantedBy = [ "multi-user.target" ];
+
+          serviceConfig = {
+            ExecStart =
+              "${cfg.package}/bin/nvidia-cdi-device-plugin " +
+              "--resource-name=${cfg.resourceName} " +
+              "--kubelet-dir=${cfg.kubeletDevicePluginDir}";
+            Restart = "always";
+            RestartSec = 5;
+          };
+        };
+      };
+    };
+  };
+}
